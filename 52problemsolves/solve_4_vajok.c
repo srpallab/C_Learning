17539dad2b22f9689cc6b6fbd437afc88a354ac4
Shafiqur Rahman <pallab.alif@gmail.com>
2019-08-25 12:15:10 +0600

    ADD: vajok solution

--- 52problemsolves/solve_4_vajok.c
+++ 52problemsolves/solve_4_vajok.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-int main()
-{
-  int i, N, m;
+int main(){
+  int i, j, N, m;
   scanf("%d", &N);
   int A[N];
   for (i = 0; i < N; ++i) {
-    A[i] = scanf("%d", &m);
+    scanf("%d", &m);
+    A[i] = m;
   }
-  for (i = 0; i < sqrt(A[i]); ++i) {
-    if (A[i] % i == 0) {
-      if (i) {
-	
+  for (i = 0; i < N; ++i) {
+    //printf("Num is: %d\n", A[i]);
+    printf("Case %d:", i+1);
+    for (j = 1; j <= A[i]; ++j) {
+      if (A[i] % j == 0) {
+	printf(" %d", j);
       }
     }
+    printf("\n");
   }
-
-
   return 0;
 }
 
