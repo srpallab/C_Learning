1c101e53cd571a221ab7266bd362d5154371464c
Shafiqur Rahman <pallab.alif@gmail.com>
2019-09-05 20:42:25 +0600

    MOD: solve 7.

--- 52problemsolves/solve_7_numberCounting.c
+++ 52problemsolves/solve_7_numberCounting.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+void input(char *str){
+  int c, i=0;
+  while((c = getchar()) != '\n'){
+    str[i++] = c;
+  }
+  str[i] = '\0';
+}
+
 int main(){
   int T;
+  char str[100];
   scanf("%d", &T);
   while (T--) {
+    input(str);
     
   }
   return 0;
